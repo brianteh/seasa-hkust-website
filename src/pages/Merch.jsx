6d3497ea3baba73a32d7b8edd9f760e74c1d90c8
Malvin <malvinrudh@gmail.com>
2025-06-16 16:41:07 +0800

    Solved resizing issues

--- src/pages/Merch.jsx
+++ src/pages/Merch.jsx
@@ -5,16 +5,17 @@ import merchImage from '/src/assets/merch/merch3.png'
 
 const Merch = () => {
     return (
-        <div className="merch-container" id ="merch">
-            <div className="image-column">
-                <img src={merchImage} alt="Merch" className="merch-image" />
+        <div className="merch-container" id="merch">
+            <div className="merch-content">
+                <div className="merch-image-container">
+                    <img src={merchImage} alt="Merch" className="merch-image" />
+                </div>
+                <div className="info-column"></div>
+                <div className="overlay">
+                    <h2 className="mainheader">OUR NEW</h2>
+                    <h2 className="subheader"><b>MERCH</b><br/>IS HERE!</h2>
+                </div>
             </div>
-            <div className="info-column"></div>
-            <div className="overlay">
-                <h2 className="mainheader">OUR NEW</h2>
-                <h2 className = "subheader"><b>MERCH</b><br/>IS HERE!</h2>
-            </div>
-
         </div>
     );
 };
--- src/pages/Merch2.jsx
+++ src/pages/Merch2.jsx
@@ -4,14 +4,23 @@ import merchImage from '/src/assets/merch/merch2.png';
 
 const Merch2 = () => {
     return (
-        <div className="merch-container1">
-            <div className="overlay1">
-                <h1 className="overlay-title1">Varsity Jacket</h1>
-                <p className="overlay-details1">Our official merchandise is now on sale.</p>
-                <button className="order-button1">Order Now</button>
-            </div>
-            <div className="image-column1">
-                <img src={merchImage} alt="Merch" className="merch-image1" />
+        <div className="merch-container1" id="merch2">
+            <div className="content-wrapper1">
+                <div className="overlay1">
+                    <h1 className="overlay-title1">Varsity Jacket</h1>
+                    <p className="overlay-details1">Our official merchandise is now on sale.</p>
+                    <a 
+                        href="https://www.youtube.com" 
+                        target="_blank" 
+                        rel="noopener noreferrer" 
+                        className="button-link"
+                    >
+                        <button className="order-button1">Order Now</button>
+                    </a>
+                </div>
+                <div className="image-column1">
+                    <img src={merchImage} alt="Merch" className="merch-image1" />
+                </div>
             </div>
         </div>
     );
