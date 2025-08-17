616b617a309a90d1fc794e10d04f9a732c706af5
Malvin <malvinrudh@gmail.com>
2025-08-17 22:09:12 +0800

    Updated the instagram and LinkedIn links

--- src/pages/Exco.jsx
+++ src/pages/Exco.jsx
@@ -7,27 +7,27 @@ import temp from "/src/assets/default.jpg"
 
 import Alicia from "/src/assets/excos/Alicia.jpg"
 const AliciaIG = "https://www.instagram.com/aliciadstny/"
-const AliciaLinkedIn = "https://www.linkedin.com" // example link, replace with actual
+const AliciaLinkedIn = "https://www.linkedin.com/in/alicia-destiny-utama"
 
 import Sophia from "/src/assets/excos/Sophia.jpg"
-const SophiaIG = "https://www.instagram.com" // example ig, replace with actual
-const SophiaLinkedIn = "https://www.linkedin.com" // example link, replace with actual
+const SophiaIG = "https://www.instagram.com/fiasssophia?igsh=MTBzeWI2M2lxbDlrZA%3D%3D&utm_source=qr"
+const SophiaLinkedIn = "https://www.linkedin.com/in/sophia-pannadi-581442325?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
 
-import Abigail from "/src/assets/excos/Abigail.jpg" // need to add image
-const AbigailIG = "https://www.instagram.com" // example ig, replace with actual
-const AbigailLinkedIn = "https://www.linkedin.com" // example link, replace with actual
+import Abigail from "/src/assets/excos/Abigail.jpg"
+const AbigailIG = "https://www.instagram.com/avrx_0405/"
+const AbigailLinkedIn = "https://www.linkedin.com/in/abigail-vun-83027233a"
 
 import Vania from "/src/assets/excos/Vania.jpg"
-const VaniaIG = "https://www.instagram.com" // example ig, replace with actual
-const VaniaLinkedIn = "https://www.linkedin.com" // example link, replace with actual
+const VaniaIG = "https://www.instagram.com/ainavsohw"
+const VaniaLinkedIn = "https://www.linkedin.com/in/aeviales"
 
 import Fiona from "/src/assets/excos/Fiona.jpg"
-const FionaIG = "https://www.instagram.com" // example ig, replace with actual
-const FionaLinkedIn = "https://www.linkedin.com" // example link, replace with actual
+const FionaIG = "https://www.instagram.com/fionakeiraa?igsh=MTR2bHZ1a2wzb285Zg%3D%3D&utm_source=qr"
+const FionaLinkedIn = "https://www.linkedin.com/in/fiona-keira-prajitno-a709891ba?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
 
-import Aliya from "/src/assets/excos/Aliya.jpg" // need to add image
-const AliyaIG = "https://www.instagram.com" // example ig, replace with actual
-const AliyaLinkedIn = "https://www.linkedin.com" // example link, replace with actual
+import Aliya from "/src/assets/excos/Aliya.jpg"
+const AliyaIG = "https://www.instagram.com/aliyacaryl/"
+const AliyaLinkedIn = "https://www.linkedin.com/in/aliya-reyes-6809b2377/"
 
 import Malvin from "/src/assets/excos/Malvin.jpg"
 const MalvinIG = "https://www.instagram.com/malvinyolo/"
@@ -35,15 +35,15 @@ const MalvinLinkedIn = "https://www.linkedin.com/in/malvin-rudh-a25b44292"
 
 import Katharine from "/src/assets/excos/Katharine.jpg" 
 const KatharineIG = "https://www.instagram.com/yur.katharine/"
-const KatharineLinkedIn = "https://www.linkedin.com" // example link, replace with actual
+const KatharineLinkedIn = "https://www.linkedin.com/in/yu-katharine?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
 
 import Carina from "/src/assets/excos/Carina.jpg"
-const CarinaIG = "https://www.instagram.com" // example ig, replace with actual
-const CarinaLinkedIn = "https://www.linkedin.com" // example link, replace with actual
+const CarinaIG = "https://www.instagram.com/carin_chandra/?next=%2F"
+const CarinaLinkedIn = "https://www.linkedin.com/in/carina-chandra-7a98b4319/"
 
 import Sidney from "/src/assets/excos/Sidney.jpg"
-const SidneyIG = "https://www.instagram.com" // example ig, replace with actual
-const SidneyLinkedIn = "https://www.linkedin.com" // example link, replace with actual
+const SidneyIG = "https://www.instagram.com/riblisidney/"
+const SidneyLinkedIn = "https://www.linkedin.com/in/sidney-ribli-49159128a/"
 
 
 
