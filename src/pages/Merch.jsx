d8bd82c617f9c98579c96ae8db8d4be381466121
Malvin <malvinrudh@gmail.com>
2025-08-17 22:09:50 +0800

    Added a mobile version layout for Merch

--- src/pages/Merch.jsx
+++ src/pages/Merch.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import '/src/pages/Merch.css' 
-
 import merchImage from '/src/assets/merch/merch3.png'
 
 const Merch = () => {
     return (
         <div className="merch-container" id="merch">
-            <div className="merch-content">
+            {/* Desktop and Tablet Layout */}
+            <div className="merch-content desktop-tablet">
                 <div className="merch-image-container">
                     <img src={merchImage} alt="Merch" className="merch-image" />
                 </div>
@@ -14,6 +14,17 @@ const Merch = () => {
                 <div className="overlay">
                     <h2 className="mainheader">OUR NEW</h2>
                     <h2 className="subheader"><b>MERCH</b><br/>IS HERE!</h2>
+                </div>
+            </div>
+
+            {/* Mobile Layout */}
+            <div className="merch-content mobile">
+                <div className="mobile-overlay">
+                    <h2 className="mobile-mainheader">OUR NEW</h2>
+                    <h2 className="mobile-subheader"><b>MERCH</b><br/>IS HERE!</h2>
+                </div>
+                <div className="mobile-image-container">
+                    <img src={merchImage} alt="Merch" className="mobile-merch-image" />
                 </div>
             </div>
         </div>
